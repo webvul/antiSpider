581961159bd216bce82087834b14793ea65bcc85
spout <snoopyxdy@gmail.com>
2015-04-17 16:27:31 +0800

    cookie

---
+++ cookie_func.lua
@@ -0,0 +1,33 @@
+require "ngx" --ngx库
+
+local config = require "config"
+local tools = require "tools"
+local ck = require "resty.cookie"
+
+
+function doSetCookie()
+		--参数
+		local args = ngx.req.get_uri_args()
+		local did = args['did']
+		if not did then
+			local resStr = tools.jsonp('0')
+			tools.jsonpSay(resStr)
+			return
+		end
+		
+		--设置加密cookie
+		local cookie, err = ck:new()
+		local ok, err = cookie:set({
+			key = config.deviceIdCookieName,
+			value = did,
+			path = "/",
+			httponly = true,
+			max_age = config.sessionMaxAge
+		})
+		local resStr = tools.jsonp('1')
+		tools.jsonpSay(resStr)
+		return
+
+end
+
+doSetCookie()
