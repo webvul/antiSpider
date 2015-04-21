0a6b04d415b566272c8b7b35817e57700d863d59
spout <snoopyxdy@gmail.com>
2015-04-21 16:36:50 +0800

    sleep coding

--- sleep_func.lua
+++ sleep_func.lua
@@ -2,14 +2,12 @@ local tools = require "tools"
 
 ngx.header["Content-Type"] = 'text/plain'
 local args = ngx.req.get_uri_args()
-local sleepArgs = args['sleep'] or '1'
-if tools.isdigit(sleepArgs) then
-	local sleep = tonumber(sleepArgs)
-	ngx.sleep(sleep)
-	ngx.say('ok')
-else
-	ngx.say('fail')
-end
+local sleepArgs = args['sleep']
+sleepArgs = tonumber(sleepArgs) or 0.1
+
+local sleep = tonumber(sleepArgs)
+ngx.sleep(sleep)
+ngx.say('ok')
 
 
 
