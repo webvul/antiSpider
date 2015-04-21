8bbba5c16cc29fde45b4c76c9f4ce7185077baa2
spout <snoopyxdy@gmail.com>
2015-04-21 13:33:59 +0800

    sleepArgs

--- sleep_func.lua
+++ sleep_func.lua
@@ -4,7 +4,7 @@ ngx.header["Content-Type"] = 'text/plain'
 local args = ngx.req.get_uri_args()
 local sleepArgs = args['sleep'] or '1'
 if tools.isdigit(sleepArgs) then
-	local sleep = tonumber()
+	local sleep = tonumber(sleepArgs)
 	ngx.sleep(sleep)
 	ngx.say('ok')
 else
