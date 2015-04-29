fd488368448df8b5fcf8b706932999917a8d05c7
spout <snoopyxdy@gmail.com>
2015-04-29 10:51:37 +0800

    print head

--- hello.lua
+++ hello.lua
@@ -1,1 +1,15 @@
-ngx.say('hello world')
+local cjson = require "cjson"
+
+ngx.header["Content-Type"] = 'text/plain'
+local args = ngx.req.get_uri_args()
+
+--如果要求打印请求头
+if printHeader then
+	local ngxHeader = ngx.req.get_headers() or {}
+	ngx.say(cjson.encode(ngxHeader))
+else
+	ngx.say('hello world')
+end
+
+
+
